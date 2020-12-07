e5e111011c3e1f53346e61e70c23cc1586060d77
James Gannon <S00196554@mail.itsligo.ie>
2020-12-07 15:19:17 +0000

    Created Employee class

---
+++ CA2/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    public abstract class Employee
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public abstract decimal CalculateMonthlyPay();
+    }
+}
